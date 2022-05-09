0ed367e5139742389e29c488a31acdfc701aec84
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-05-09 17:41:54 +0200

    feat: crear interfaz ICalculadora

---
+++ src/main/java/edu/poniperro/ICalculadora.java
@@ -0,0 +1,8 @@
+package edu.poniperro;
+
+public interface ICalculadora {
+    double sumar(double sumandoA, double sumandoB);
+    double restar(double minuendo, double sustraendo);
+    double multiplicar(double factorA, double factorB);
+    double dividir(double dividendo, double divisor);
+}
